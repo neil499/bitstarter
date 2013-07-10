ac2c24f6b57c2220887e7b51bec14c46b4b79a4a
neil499 <neil499@startup.(none)>
2013-07-09 20:12:39 -0400

    First part...

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var str = fs.readFileSync('index.html','utf8');    
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
